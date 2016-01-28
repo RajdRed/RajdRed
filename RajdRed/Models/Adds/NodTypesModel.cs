9a6519d21378f54733765103dbd10fd7d6416ee4
Alexnyr <alex.nyrot@gmail.com>
2016-01-28 09:58:49 +0100

    Lagt till NodTypes

--- RajdRed/Models/Adds/NodTypesModel.cs
+++ RajdRed/Models/Adds/NodTypesModel.cs
@@ -1,32 +1,34 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Media;
 
 namespace RajdRed.Models.Adds
 {
     class NodTypesModel
     {
-        public static Geometry Association()
+        public GeometryGroup Node = new GeometryGroup()
         {
-            PathGeometry geometry = new PathGeometry();
-            PathFigure figure = new PathFigure();
-            figure.StartPoint = new Point(2, 5);
-            figure.Segments.Add(new PolyLineSegment()
-            {
-                Points = new PointCollection()
-                {
-                   new Point(7,5),
-                   new Point(5,2),
-                   new Point(5,7)
+            Children = new GeometryCollection(){
+                new LineGeometry(new Point(5, 1), new Point(5, 9)),
+                new LineGeometry(new Point(1, 5), new Point(9, 5))
                 }
+        };
+
+        public EllipseGeometry Association = new EllipseGeometry(new Point(5, 5), 5, 5);
+
+        public RectangleGeometry Aggregation = new RectangleGeometry(new Rect(new Size(5, 5)), 0, 0, new RotateTransform(45));
+
+        public PathGeometry Generalization = new PathGeometry(
+            new List<PathFigure>() 
+            { 
+                new PathFigure(new Point(4.5, 1), 
+                new List<PathSegment>()
+                {
+                    new LineSegment(new Point(4.5, 1), true),
+                    new LineSegment(new Point(9, 9), true),
+                    new LineSegment(new Point(1, 9), true)
+                }, 
+                true) 
             });
-
-            geometry.Figures.Add(figure);
-            return geometry;
-        }
-
-        public static Geometry Aggregation()
-        {
-            return new RectangleGeometry(new Rect(new Size(10, 10)));
-        }
     }
 }
